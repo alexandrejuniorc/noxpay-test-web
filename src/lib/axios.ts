b8ded7d8a251802854fb4959153ea92812f9f85e
Alexandre Junior <alexandrejunior408@gmail.com>
2023-10-09 00:55:16 -0300

    fix: change paths on routes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,22 +3,15 @@ import axios from "axios";
 export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
 
 export const getAllCriptos = async () => {
-  const getAllCriptos = await api
-    .get("/criptocurrency")
-    .then((response) => response.data);
-  return getAllCriptos;
+  return await api.get("/cryptocurrency").then((response) => response.data);
 };
-
 export const createNewCripto = async (data: any) => {
-  const createNewCripto = await api
-    .post("/criptocurrency", data)
+  return await api
+    .post("/cryptocurrency", data)
     .then((response) => response.data);
-  return createNewCripto;
 };
-
 export const updateVoteCripto = async (id: string) => {
-  const updateVoteCripto = await api
-    .put(`/criptocurrency/${id}/vote`)
+  return await api
+    .put(`/cryptocurrency/${id}/vote`)
     .then((response) => response.data);
-  return updateVoteCripto;
 };
